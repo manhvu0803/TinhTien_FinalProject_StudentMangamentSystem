21d99e32bfe6b89e571c2783c527e321513fe2ef
Samtron45b <tien.nnang@gmail.com>
2020-04-15 06:07:06 +0700

    Update save/scan +Upload Edit

--- finalproject/course.cpp
+++ finalproject/course.cpp
@@ -1,4 +1,4 @@
-#include "course.h"
+﻿#include "course.h"
 
 void scanCourse(ifstream& myFile, vector<Course>& List)
 {
@@ -44,15 +44,15 @@ void scanCourse(ifstream& myFile, vector<Course>& List)
 }
 void saveCourse(ofstream& myFile, vector<Course>List)
 {
-	myFile << "'No," << "Id," << "Name," << "Class,"
+	myFile << "No," << "Id," << "Name," << "Class,"
 		<< "Account," << "startDate," << "endDate,"
 		<< "DoW," << "startHour," << "endHour," << "Room" << endl;
-	for (int i = 0; i < List.size(); i++)
+	for (int i = 0; i < List.size(); ++i)
 	{
-		myFile << List[i].No << List[i].Id << "," << List[i].Name << ","
-			<< List[i].Class << "," << List[i].Account << "," << List[i].startDate
-			<< "," << List[i].endDate << "," << List[i].DoW << "," << List[i].startHour
-			<< "," << List[i].endHour << "," << List[i].Room << endl;
+		myFile <<  List[i].No<<","<<List[i].Id << "," << List[i].Name << ","
+			   << List[i].Class << "," << List[i].Account << "," << List[i].startDate
+		       << "," << List[i].endDate << "," << List[i].DoW << "," << List[i].startHour
+			   << "," << List[i].endHour << "," << List[i].Room << endl;
 	}
 }
 void input1Course(vector<Course>& List)
@@ -73,12 +73,28 @@ void input1Course(vector<Course>& List)
 void inputCourse(vector<Course>& List, int& n)
 {
 	cout << "How many course: "; cin >> n;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; ++i)
 	{
 		cout << "         No : " << i + 1 << endl;
 		List[i].No = i;
 		input1Course(List);
 	}
+	sortList(List);
+}
+void output1Course(Course course)
+{
+	cout << course.No
+		<< setw(10) << course.Id
+		<< setw(25) << course.Name
+		<< setw(10) << course.Class
+		<< setw(15) << course.Account
+		<< setw(20) << course.startDate
+		<< setw(17) << course.endDate
+		<< setw(10) << course.DoW
+		<< setw(15) << course.startHour
+		<< setw(12) << course.endHour
+		<< setw(12) << course.Room
+		<< endl;
 }
 void outputCourse(vector<Course> List)
 {
@@ -86,19 +102,104 @@ void outputCourse(vector<Course> List)
 		<< setw(21) << "Lecturer Account" << setw(15) << "Start date" << setw(15)
 		<< "End date" << setw(15) << "Day of Week" << setw(15) << "Start hour"
 		<< setw(10) << "End hour" << setw(10) << "Room" << endl;
-	for (int i = 0; i < List.size(); i++)
+	for (int i = 0; i < List.size(); ++i)
 	{
-		cout << List[i].No
-			<< setw(10) << List[i].Id
-			<< setw(25) << List[i].Name
-			<< setw(10) << List[i].Class
-			<< setw(15) << List[i].Account
-			<< setw(20) << List[i].startDate
-			<< setw(17) << List[i].endDate
-			<< setw(10) << List[i].DoW
-			<< setw(15) << List[i].startHour
-			<< setw(12) << List[i].endHour
-			<< setw(12) << List[i].Room
-			<< endl;
+		output1Course(List[i]);
+	}
+}
+void delete1Course(vector<Course>& List, int n)// n là No của course
+{
+	List.erase(List.begin() + n - 1);
+}
+void editCourse(vector<Course>& List, int n)// n là No của course
+{
+	int choice;
+	cout << " (0) No" << endl;
+	cout << " (1) Id" << endl;
+	cout << " (2) Name" << endl;
+	cout << " (3) Class" << endl;
+	cout << " (4) Account" << endl;
+	cout << " (5) Start date" << endl;
+	cout << " (6) End date" << endl;
+	cout << " (7) Day of week" << endl;
+	cout << " (8) Start hour" << endl;
+	cout << " (9) End hour" << endl;
+	cout << " (10) Room" << endl;
+	while (true)
+	{
+		cout << " Change : ";
+		cin >> choice;
+		if (choice <= 10 && choice >= 0)
+		{
+			break;
+		}
+		else
+		{
+			cout << "Error"<<endl;
+		}
+	}
+	cin.ignore(1);
+	switch (choice)
+	{
+	default:
+		cout << " No : "; cin >> List[n].No;
+	case 1:
+		cout << " Id : "; getline(cin, List[n].Id);
+		break;
+	case 2:
+		cout << " Name :"; getline(cin, List[n].Name);
+		break;
+	case 3:
+		cout << " Class : "; getline(cin, List[n].Class);
+		break;
+	case 4:
+		cout << " Account : "; getline(cin, List[n].Account);
+		break;
+	case 5:
+		cout << " Start date : "; getline(cin, List[n].startDate);
+		break;
+	case 6:
+		cout << " End date : "; getline(cin, List[n].endDate);
+		break;
+	case 7:
+		cout << " Day of week : "; getline(cin, List[n].DoW);
+		break;
+	case 8:
+		cout << " Start hour : "; getline(cin, List[n].startHour);
+		break;
+	case 9:
+		cout << " End hour    : "; getline(cin, List[n].endHour);
+		break;
+	case 10:
+		cout << " Room        : "; getline(cin, List[n].Room);
+		break;
+	}
+	ofstream myFile("course.csv");
+	if (myFile.is_open())
+	{
+		cout << "After edit: " << endl;
+		output1Course(List[n]);
+		sortList(List);
+		saveCourse(myFile, List);
+	}
+	else
+	{
+		cout << "Error saving edit";
+	}
+	myFile.close();
+}
+void sortList(vector<Course>& List)
+{
+	sort(List.begin(), List.end(), [](const Course& lhs, const Course& rhs)
+		{
+			return lhs.Id < rhs.Id;
+		});
+	sort(List.begin(), List.end(), [](const Course& lhs, const Course& rhs)
+		{
+			return lhs.Name < rhs.Name;
+		});
+	for (int i = 0; i < List.size(); ++i)
+	{
+		List[i].No = i + 1;
 	}
 }
