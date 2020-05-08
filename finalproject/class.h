cf44f502ff9fb19b2ada4e4b8cf667f98fba9174
manhvu0803 <31726319+manhvu0803@users.noreply.github.com>
2020-05-08 17:40:42 +0700

    Reset

--- finalproject/class.h
+++ finalproject/class.h
@@ -1,114 +1,18 @@
 #ifndef CLASS_H_INCLUDE
 #define CLASS_H_INCLUDE
 
-#include<iostream>
-#include<fstream>
-#include<iomanip>
-//#include<string>
-#include<sstream>
-#include<vector>
-#include"dataStructure.h"
-//#include"utility.h"
+#include <iostream>
+#include <string>
+#include "dataStructure.h"
+#include "utility.h"
+
 using namespace std;
-//using namespace tt;
 
-/*struct date {
-    int m = 0, d = 0, y = 0;
+class stdnt
+{
+    public:
+
+    private:
 };
-
-struct student {
-    int number = 0;
-    int id = 0;
-    string lastName, firstName;
-    char gender = '0';
-    date DoB;
-};*/
-
-int MaxDayInMonth(int month, int year);
-
-//add new student one by one
-void AddOneStudent(vector<tt::student>& stL);
-
- //find and show information of a student that has the given id in the course.
-//(if thar student study in more than 1 class->show information more than one time)
-void FindStudentInAllClass(vector<tt::student>& stL, int id, int& check, tt::student& st, string& classname, int& count);
-
-//Input new data of new class or reinput all data of the existing class.
-void CreateNewClass(vector<tt::student>& stL, int& n);
-
-//load from csv file.
-void LoadFromCSVFile(ifstream& fload, vector<tt::student>& stL);
-
-//do the import action with the csv file that has the given name.
-void ImportToProgram(vector<tt::student>&stL, string flname);
-
-//view information of 1 student.
-void DisPlay1Student(tt::student st);
-
-//VIEW INFORMATION OF 1 STUDENT LINE BY LINE.
-void DisPlay1StudentLine(tt::student st);
-
-//view one class.
-void DisPlayClass(vector<tt::student> stL);
-
-//VIEW ONG CLASS LINE BY LINE.
-void DisPlayClassLine(vector<tt::student> stL);
-
-//view all the class in course.
-void DisPlayAllClass(vector<tt::student>stL);
-
-//VIEW ALL THE CLASS IN COURSE LINE BY LINE.
-void DisPlayAllClassLine(vector<tt::student>stL);
-
-//change the information of a student that has the given id.
-void Edit1Student(tt::student &st);
-
-// update the change of the information of a student that has the given id in his/her class.
-void UpateIn1Class(vector<tt::student>& stL, int number, tt::student st);
-
-//update the change of the information of a student that has the given id in all his/her classes.
-void UpateInAllClass(vector<tt::student>& stL, int id, tt::student st);
-
-//remove a student that has the given id in the class has the given class's name.
-void RemoveStudentFrom1Class(vector<tt::student> &stL, int id, string classname);
-//remove a student in all of his / her classes.
-void RemoveStudentFromAllClass(vector<tt::student>stL, int id);
-
-//erase a student that has the given id from class A and put into class B.
-void ChangeStudentFromClassAToClassB(vector<tt::student>& stLA, vector<tt::student>& stLB, int number, int& check, int id);
-
-//save to csv file.
-void ExportToCSVFile(ofstream& fsave, vector<tt::student> stL);
-
-//do the save to csv file.
-void ExportFromProgram(vector<tt::student>stL, string fsname);
-
-//find a student that has the given id in just only the class that has class's name given.
-void FindStudentIn1Class(vector<tt::student> stL, int id, string classname, int& check, tt::student& st, int& count);
-
-//load from dat file.
-void LoadFromDATFile(ifstream& fload, vector<tt::student>& stL);
-
-//do the load action.
-void ImportToProgram2(vector<tt::student>& stL, string flname);
-
-//save to dat file.
-void SaveToDATFile(ofstream& fsave, vector<tt::student> stL);
-
-//do the save to dat file.
-void ExportFromProgram2(vector<tt::student> stL, string fsname);
-
-//sort the class in ascending order.
-void SortClassInAscendingOrderByID(vector<tt::student>& stL);
-
-//check if the name of the class is saved or not.
-int CheckClassnameSavedorNot(string classname);
-
-//add new class name.
-void AddClassname(string classname);
-
-
-//CLASS MANAGE MENU.
-void ManageClassFunction();
 
 #endif
