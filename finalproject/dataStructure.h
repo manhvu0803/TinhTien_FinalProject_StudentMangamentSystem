11c5f7c44f56f872d54ea1422c5dbd6a6bd7a479
manhvu0803 <31726319+manhvu0803@users.noreply.github.com>
2020-05-10 23:49:09 +0700

    Add initialize value for everything

--- finalproject/dataStructure.h
+++ finalproject/dataStructure.h
@@ -8,59 +8,57 @@ namespace tt
 {
     struct date
     {
-        int d = 0, m = 0, y = 0;
+        int d = -1, m = -1, y = -1;
     };
 
     struct time
     {
-        int h, m, s;
+        int h = -1, m = -1, s = -1;
     };
 
     struct lecturer
     {
-        int number;
-        std::string username;
-        std::string fullName;
-        std::string degree;
-        char gender; // 'M' for male, 'F' for female
+        int number = -1;
+        std::string username = "";
+        std::string fullName = "";
+        std::string degree = "";
+        char gender = 'T'; // 'M' for male, 'F' for female
     };
 
     struct student
     {
-        int number;
-        int id;
-        std::string lastName, firstName;
-        char gender; // 'M' for male, 'F' for female
+        int number = -1;
+        int id = -1;
+        std::string lastName = "", firstName = "";
+        char gender = 'T'; // 'M' for male, 'F' for female
         date DoB; // date of birth
     };
 
     struct score
     {
-        std::string cls; // class of the student
-        std::string studentName;
-        int id; // ID of the student this score belongs to
-        float mid, final, bonus, total;
-        tt::vector<date> checkedDate;
-        tt::vector<time> checkedTime;
+        std::string cls = ""; // class of the student
+        std::string studentName = "";
+        int id = -1; // ID of the student this score belongs to
+        float mid = -1, final = -1, bonus = -1, total = -1;
     };
 
     struct course
     {
-        int number;
-        std::string id;
-        std::string name;
-        std::string className;
-        std::string lecturer;
-        int DoW; // date of week, Monday is 0, Tuesday is 1,...
+        int number = -1;
+        std::string id = "";
+        std::string name = "";
+        std::string className = "";
+        std::string lecturer = "";
+        int DoW = -1; // date of week, Monday is 0, Tuesday is 1,...
         date startDate, endDate;
         time startTime, endTime;
         //tt::vector<student *> students;
-        std::string room;
+        std::string room = "";
     };
 
     struct checkIn
     {
-        int id;
+        int id = -1;
         date cDate;
         time cTime;
     };
